88a30680e4f0ba18ca124e62b9dc78a35ffcfc55
Nikita Nizhankovskiy <eegby@yandex.ru>
2025-11-04 08:47:29 +0300

    Refactored nextdate module, successfull tests.

--- pkg/api/nextdate.go
+++ pkg/api/nextdate.go
@@ -8,12 +8,10 @@ import (
 	"time"
 )
 
-const (
-	maxDays int = 400
-	layout      = "20060102"
-)
+var maxDays int = 400
+var layout = "20060102"
 
-// nextDayHandler handles Get-request with repeat pattern and responses next date
+// nextDayHandler handles Get-request with repeat pattern and responses next date.
 func nextDayHandler(res http.ResponseWriter, req *http.Request) {
 	date := req.FormValue("date")
 	repeat := req.FormValue("repeat")
@@ -38,13 +36,186 @@ func nextDayHandler(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte(next))
 }
 
-// daysIn calculates the number of days in the passed month of passed year
+// NextDate calculates next date with different patterns, and dates.
+func NextDate(now time.Time, dstart string, repeat string) (string, error) {
+	nextTime, err := time.Parse(layout, dstart)
+	if err != nil {
+		return "", fmt.Errorf("can't parse start date parameter - wrong input; %e", err)
+	}
+	if repeat == "" {
+		return "", fmt.Errorf("can't parse repeat parameter - empty input")
+	}
+	repeatData := strings.Fields(repeat)
+	if len(repeatData) == 0 {
+		return "", fmt.Errorf("can't parse repeat parameter - wrong input")
+	}
+
+	switch repeatData[0] {
+	case "d":
+		return dailyCalc(repeatData, now, nextTime)
+	case "y":
+		return yearlyCalc(now, nextTime)
+	case "w":
+		return weeklyCalc(repeatData, now, nextTime)
+	case "m":
+		return monthlyCalc(repeatData, now, nextTime)
+	default:
+		return "", fmt.Errorf("can't use repeat parameter - undefined parameter %q", repeatData[0])
+	}
+}
+
+// dailyCalc calculates next date with daily pattern.
+func dailyCalc(repeatData []string, now, nextTime time.Time) (string, error) {
+	if len(repeatData) == 1 {
+		return "", fmt.Errorf("can't shift date - no parameters in daily pattern")
+	}
+	shift, err := strconv.Atoi(repeatData[1])
+	if err != nil {
+		return "", fmt.Errorf("days parameter: unsupported format; %e", err)
+	}
+	if shift > maxDays || shift <= 0 {
+		return "", fmt.Errorf("days parameter: wrong input (%d)", shift)
+	}
+	for {
+		//wrong logic - now: 20240126 dstart: 20240202 repeat: d 30; nextTime: 20240303?
+		nextTime = nextTime.AddDate(0, 0, shift)
+		if isAfter(nextTime, now) {
+			break
+		}
+	}
+	return nextTime.Format(layout), nil
+}
+
+// yearlyCalc calculates next date with yearly pattern.
+func yearlyCalc(now, nextTime time.Time) (string, error) {
+	for {
+		nextTime = nextTime.AddDate(1, 0, 0)
+		if isAfter(nextTime, now) {
+			break
+		}
+	}
+	return nextTime.Format(layout), nil
+}
+
+// weeklyCalc calculates next date with weekly pattern. It builds a map with valid weekdays and then validate it in cycle.
+func weeklyCalc(repeatData []string, now, nextTime time.Time) (string, error) {
+	if len(repeatData) == 1 {
+		return "", fmt.Errorf("can't shift date - no parameters in weekly pattern")
+	}
+	days := strings.Split(repeatData[1], ",")
+	weekDays := make(map[int]bool, len(days))
+	for _, d := range days {
+		dayNum, err := strconv.Atoi(d)
+		if err != nil {
+			return "", fmt.Errorf("weekday parameter: unsupported format; %e", err)
+		}
+		if dayNum < 1 || dayNum > 7 {
+			return "", fmt.Errorf("weekday parameter: wrong input (%d)", dayNum)
+		}
+		if dayNum == 7 {
+			dayNum = 0
+		}
+		weekDays[dayNum] = true
+	}
+	for {
+		dayNum := int(nextTime.Weekday())
+		if weekDays[dayNum] && isAfter(nextTime, now) {
+			break
+		}
+		nextTime = nextTime.AddDate(0, 0, 1)
+	}
+	return nextTime.Format(layout), nil
+}
+
+// monthlyCalc calculates next date with monthly pattern. It uses a maps parsed monthlyParser
+// and then validate it in cycles.
+func monthlyCalc(repeatData []string, now, nextTime time.Time) (string, error) {
+	daysCurrMonth := daysIn(int(nextTime.Month()), nextTime.Year())
+	dayNums, monthNums, err := monthlyParser(repeatData, daysCurrMonth)
+	if err != nil {
+		return "", fmt.Errorf("monthlyParser error: %e", err)
+	}
+
+	if dayNums == nil && monthNums == nil {
+		return "", fmt.Errorf("monthlyParser error: parse fail")
+	}
+
+	if dayNums != nil && monthNums == nil {
+		for {
+			dayNum := nextTime.Day()
+			if dayNums[dayNum] && isAfter(nextTime, now) {
+				break
+			}
+			nextTime = nextTime.AddDate(0, 0, 1)
+		}
+		return nextTime.Format(layout), nil
+	}
+
+	for {
+		dayNum := nextTime.Day()
+		monthNum := nextTime.Month()
+		if monthNums[int(monthNum)] && dayNums[dayNum] && isAfter(nextTime, now) {
+			break
+		}
+		nextTime = nextTime.AddDate(0, 0, 1)
+	}
+	return nextTime.Format(layout), nil
+
+}
+
+// monthlyParser parses and checks monthly string patterns and writes a maps with valid day numbers and month numbers
+func monthlyParser(repeatData []string, daysThisMonth int) (map[int]bool, map[int]bool, error) {
+
+	if len(repeatData) == 1 {
+		return nil, nil, fmt.Errorf("can't shift date - no parameters in monthly pattern")
+	}
+	daysParse := strings.Split(repeatData[1], ",")
+	dayNums := make(map[int]bool, len(daysParse))
+	for _, d := range daysParse {
+		date, err := strconv.Atoi(d)
+		if err != nil {
+			return nil, nil, fmt.Errorf("date parameter: unsupported format; %e", err)
+		}
+		if date < -2 || date == 0 || date > 31 {
+			return nil, nil, fmt.Errorf("date parameter: wrong input (%d)", date)
+		}
+		// if user set task to a 31th day with monthly pattern - he will find it at the last day of the next month, anyway.
+		if date > daysThisMonth {
+			dayNums[daysThisMonth] = true
+			continue
+		}
+		if date < 0 {
+			dayNums[daysThisMonth+date+1] = true
+			continue
+		}
+		dayNums[date] = true
+	}
+	if len(repeatData) == 3 {
+		monthesParse := strings.Split(repeatData[2], ",")
+		monthNums := make(map[int]bool, len(monthesParse))
+
+		for _, m := range monthesParse {
+			month, err := strconv.Atoi(m)
+			if err != nil {
+				return nil, nil, fmt.Errorf("month parameter - unsupported format; %e", err)
+			}
+			if month < 0 || month > 12 {
+				return nil, nil, fmt.Errorf("month parameter: wrong input (%d)", month)
+			}
+			monthNums[month] = true
+		}
+		return dayNums, monthNums, nil
+	}
+	return dayNums, nil, nil
+}
+
+// daysIn calculates the number of days in the passed month of passed year.
 func daysIn(month, year int) int {
 	t := time.Date(year, time.Month(month)+1, 0, 0, 0, 0, 0, time.UTC)
 	return t.Day()
 }
 
-// sameDay returns true if date1 is same or after date2
+// isAfter returns true if date1 is after date2.
 func isAfter(date1, date2 time.Time) bool {
 	y1, m1, d1 := date1.Date()
 	y2, m2, d2 := date2.Date()
@@ -55,146 +226,4 @@ func isAfter(date1, date2 time.Time) bool {
 		return m1 > m2
 	}
 	return d1 > d2
-}
-
-// NextDate calculates next date with repeat pattern, now and start dates
-func NextDate(now time.Time, dstart string, repeat string) (string, error) {
-	nextTime, err := time.Parse(layout, dstart)
-	if err != nil {
-		return "", fmt.Errorf("%e:\ncan't parse dstart parameter - wrong input", err)
-	}
-
-	if repeat == "" {
-		return "", fmt.Errorf("can't parse repeat parameter - empty input")
-	}
-	repeatData := strings.Split(repeat, " ")
-	if len(repeatData) == 0 {
-		return "", fmt.Errorf("can't parse repeat parameter - wrong input")
-	}
-
-	switch repeatData[0] {
-
-	case "d":
-		if len(repeatData) == 1 {
-			return "", fmt.Errorf("can't shift date - no d's parameters on input")
-		}
-		shift, err := strconv.Atoi(repeatData[1])
-		if err != nil {
-			return "", fmt.Errorf("%e:\ncan't parse days shift parameter", err)
-		}
-		if shift > maxDays || shift < 0 {
-			return "", fmt.Errorf("can't shift date - wrong input (%d) days", shift)
-		}
-		for {
-			//wrong logic - now: 20240126 dstart: 20240202 repeat: d 30; nextTime: 20240303?
-			nextTime = nextTime.AddDate(0, 0, shift)
-			if isAfter(nextTime, now) {
-				break
-			}
-		}
-
-	case "y":
-		for {
-			nextTime = nextTime.AddDate(1, 0, 0)
-			if isAfter(nextTime, now) {
-				break
-			}
-		}
-
-	case "w":
-		if len(repeatData) == 1 {
-			return "", fmt.Errorf("can't shift date - no w's parameters on input")
-		}
-		days := strings.Split(repeatData[1], ",")
-		weekDays := make(map[int]bool, len(days))
-		for _, d := range days {
-			dayNum, err := strconv.Atoi(d)
-			if err != nil {
-				return "", fmt.Errorf("%e:\ncan't parse weekday parameter - unsupported format", err)
-			}
-			if dayNum < 1 || dayNum > 7 {
-				return "", fmt.Errorf("can't use weekday parameter - wrong input")
-			}
-			if dayNum == 7 {
-				dayNum = 0
-			}
-			weekDays[dayNum] = true
-		}
-		for {
-			if weekDays[int(nextTime.Weekday())] && isAfter(nextTime, now) {
-				break
-			}
-			nextTime = nextTime.AddDate(0, 0, 1)
-		}
-
-	case "m":
-		if len(repeatData) == 1 {
-			return "", fmt.Errorf("can't shift date - no m's []<> parameters on input")
-		}
-		daysParse := strings.Split(repeatData[1], ",")
-		days := make(map[int]bool, len(daysParse))
-		daysTheseMonth := daysIn(int(nextTime.Month()), nextTime.Year())
-		if len(repeatData) > 1 {
-			for _, d := range daysParse {
-				date, err := strconv.Atoi(d)
-				if err != nil {
-					return "", fmt.Errorf("%e:\ncan't parse m <date> parameter - unsupported format", err)
-				}
-				if date < -2 || date > 31 {
-					return "", fmt.Errorf("can't use m <date> parameter - wrong input (%d)", date)
-				}
-
-				if date > daysTheseMonth {
-					days[daysTheseMonth] = true
-					continue
-				}
-				if date < 0 {
-					days[daysTheseMonth+date+1] = true
-					continue
-				}
-				days[date] = true
-			}
-
-			if len(repeatData) == 2 {
-				for {
-					if days[int(nextTime.Day())] && isAfter(nextTime, now) {
-						break
-					}
-					nextTime = nextTime.AddDate(0, 0, 1)
-				}
-				return nextTime.Format(layout), nil
-			}
-
-			if len(repeatData) == 3 {
-				monthesParse := strings.Split(repeatData[2], ",")
-				monthes := make(map[int]bool, len(monthesParse))
-
-				for _, m := range monthesParse {
-					month, err := strconv.Atoi(m)
-					if err != nil {
-						return "", fmt.Errorf("%e:\ncan't parse m [month] parameter - unsupported format", err)
-					}
-					if month < 0 || month > 12 {
-						return "", fmt.Errorf("can't use m [month] parameter - wrong input (%d)", month)
-					}
-					monthes[month] = true
-				}
-
-				for {
-					nextTime = nextTime.AddDate(0, 0, 1)
-					if monthes[int(nextTime.Month())] &&
-						days[int(nextTime.Day())] &&
-						isAfter(nextTime, now) {
-						break
-					}
-				}
-				return nextTime.Format(layout), nil
-			}
-		}
-
-	default:
-		return "", fmt.Errorf("can't use repeat parameter - undefined parameter (%q)", repeatData[0])
-	}
-
-	return nextTime.Format(layout), nil
 }
